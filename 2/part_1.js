bd785baa76a54b7da8ac14c02b1518b23c59b94d
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-12-02 12:05:35 +0000

    Complete day 2

---
+++ 2/part_1.js
@@ -0,0 +1,26 @@
+import { games } from './data.js';
+
+const COLOR_LIMITS = {
+    RED: 12,
+    GREEN: 13,
+    BLUE: 14,
+};
+
+const validGameIDs = [];
+
+games.forEach((game, i) => {
+    const redCounts = game.match(/\d+(?=\sred)/g) ?? [];
+    const greenCounts = game.match(/\d+(?=\sgreen)/g) ?? [];
+    const blueCounts = game.match(/\d+(?=\sblue)/g) ?? [];
+
+    if (
+        redCounts.every((count) => +count <= COLOR_LIMITS.RED) &&
+        greenCounts.every((count) => +count <= COLOR_LIMITS.GREEN) &&
+        blueCounts.every((count) => +count <= COLOR_LIMITS.BLUE)
+    ) {
+        validGameIDs.push(i + 1);
+    }
+});
+
+const sumValidIDs = validGameIDs.reduce((a, c) => a + c);
+console.log(sumValidIDs);
---
+++ 2/part_2.js
@@ -0,0 +1,18 @@
+import { games } from './data.js';
+
+const cubePowers = [];
+
+function toCubePower(red, green, blue) {
+    return Math.max(...red) * Math.max(...green) * Math.max(...blue);
+}
+
+games.forEach((game, i) => {
+    const redCounts = game.match(/\d+(?=\sred)/g) ?? [];
+    const greenCounts = game.match(/\d+(?=\sgreen)/g) ?? [];
+    const blueCounts = game.match(/\d+(?=\sblue)/g) ?? [];
+
+    cubePowers.push(toCubePower(redCounts, greenCounts, blueCounts));
+});
+
+const sumCubePowers = cubePowers.reduce((a, c) => a + c);
+console.log(sumCubePowers);
