1edade6c1a80e5a13727ac7fbe62c1ff90f6583f
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-12-05 21:24:30 +0000

    Add param types and delimiter default param

--- readFile.js
+++ readFile.js
@@ -2,9 +2,16 @@ import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 
-export const dataToStringArray = (fileName, metaURL) => {
+/**
+ *
+ * @param {string} fileName
+ * @param {string} metaURL
+ * @param {string} delimiter '\n'
+ * @returns string[]
+ */
+export const dataToStringArray = (fileName, metaURL, delimiter = '\n') => {
     const __dirname = path.dirname(fileURLToPath(metaURL));
     const dataString = readFileSync(path.join(__dirname, fileName), { encoding: 'utf8' });
 
-    return dataString.split('\n');
+    return dataString.split(delimiter);
 };
